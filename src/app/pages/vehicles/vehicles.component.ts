acf52a12c5f3e8badad838e02d0589620b4b8ac2
sniperdev <igorspychalaa@gmail.com>
2024-03-07 15:09:53 +0100

    <feat> add content do vehicles component

--- src/app/pages/vehicles/vehicles.component.ts
+++ src/app/pages/vehicles/vehicles.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {IVehicle} from "../../../store/vehicles/interfaces/vehicles.interface";
+import {VehiclesFacade} from "../../../store/vehicles/vehicles.facade";
 
 @Component({
   selector: 'app-vehicles',
   templateUrl: './vehicles.component.html',
   styleUrls: ['./vehicles.component.scss']
 })
-export class VehiclesComponent {
+export class VehiclesComponent implements OnInit{
+  public vehicles$: Observable<IVehicle[]> = this.vehiclesFacade.items$;
 
+  constructor(private vehiclesFacade: VehiclesFacade){}
+
+  ngOnInit() {
+    this.vehiclesFacade.loadVehiclesList();
+  }
 }
--- src/app/pages/vehicles/vehicles.module.ts
+++ src/app/pages/vehicles/vehicles.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {VehiclesComponent} from "./vehicles.component";
 import { VehiclesRoutingModule } from './vehicles-routing.module';
+import {SharedModule} from "../../shared/shared.module";
 
 
 
@@ -11,7 +12,8 @@ import { VehiclesRoutingModule } from './vehicles-routing.module';
   ],
   imports: [
     CommonModule,
-    VehiclesRoutingModule
+    VehiclesRoutingModule,
+    SharedModule
   ]
 })
 export class VehiclesModule { }
