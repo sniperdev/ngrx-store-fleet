6e5407fde9f0e72651f2ff74e990001debe0542d
sniperdev <igorspychalaa@gmail.com>
2024-03-14 20:10:03 +0100

    <feat> more ux column name

--- src/app/shared/table/table.component.ts
+++ src/app/shared/table/table.component.ts
@@ -6,14 +6,19 @@ import {Component, Input} from '@angular/core';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent<T extends object> {
-  protected cols!: string[];
+  protected cols!: any;
   private _items!: T[];
 
   @Input()
   set items(items: T[]) {
     this._items = items;
     if (items && items.length > 0) {
-      this.cols = Object.keys(items[0]);
+      let colsValues: string[] = Object.keys(items[0]);
+      this.cols = colsValues.map((col: string) => {
+        let formattedCol = col.charAt(0).toUpperCase() + col.slice(1);
+        formattedCol = formattedCol.replace(/([a-z])([A-Z])/g, '$1 $2');
+        return { field: col, header: formattedCol };
+      });
     }
   }
 
