42d746f6c6d5a20e24c9abac57a128dde178c248
sniperdev <igorspychalaa@gmail.com>
2024-03-14 18:52:55 +0100

    <feat> routing to orders page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,11 @@ const routes: Routes = [
     path: 'drivers',
     loadChildren: () =>
       import('./pages/drivers/drivers.module').then((m) => m.DriversModule)
+  },
+  {
+    path: 'orders',
+    loadChildren: () =>
+      import('./pages/orders/orders.module').then((m) => m.OrdersModule)
   }
 ];
 
