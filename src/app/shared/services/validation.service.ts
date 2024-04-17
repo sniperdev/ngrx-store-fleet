4f910e26f9b3c7be3429818c0c3e056a76d09ee2
sniperdev <igorspychalaa@gmail.com>
2024-04-17 18:56:12 +0200

    <feat> add more messages to validation service

--- src/app/shared/services/validation.service.ts
+++ src/app/shared/services/validation.service.ts
@@ -18,6 +18,12 @@ export class ValidationService {
   private getValidationMessage(key: string, name?: string){
     const messages = {
       required: "This field is required.",
+      email: "Please enter a valid email address.",
+      minLength: `Please enter at least 8 characters.`,
+      maxLength: `Please enter no more than 32 characters.`,
+      pattern: `The input does not match the expected pattern.`,
+      number: `Please enter a valid number.`,
+      date: `Please enter a valid date.`,
     };
     return messages[key as keyof object];
   }
